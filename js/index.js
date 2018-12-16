30dd72b72e564e1ca50dcb56e826a2c71b2ff98a
Connor Callahan <connorwcallahan@gmail.com>
2018-12-16 16:39:08 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,29 @@
-//this file is optional
-//you can use this to paste in js code and have it run when index.html is loaded
+
+
+const someElement = document.createElement('div')
+
+someElement.innerHTML = 'Hello, DOM!'
+someElement.style.backgroundColor = 'grey'
+
+document.body.appendChild(someElement)
+
+someElement.style.textAlign = 'center';
+
+
+const ul = document.createElement('ul');
+
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+
+someElement.appendChild(ul);
+
+ul.style.textAlign = 'left';
+
+ul.removeChild(ul.querySelector('li:nth-child(2)'));
+
+ul.remove();
+
+//testing
